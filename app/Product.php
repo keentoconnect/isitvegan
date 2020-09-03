e5b895374a76a00ebfb1de1d437f408c037b41c3
Michael Brooks <me@michaelbrooks.co.uk>
2020-09-03 09:13:37 +0100

    Fixed product page

--- app/Product.php
+++ app/Product.php
@@ -3,12 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 
 class Product extends Model
 {
-    use Searchable;
-
     public function asset()
     {
         return $this->belongsTo(Asset::class);
